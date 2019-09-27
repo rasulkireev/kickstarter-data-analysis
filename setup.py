9aa1907df4f0874f9360c42f433575fbc17bb2e1
Rasul Kireev <kireevr1996@gmail.com>
2019-09-27 15:45:16 -0400

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing Kickstarter data to predict the success of any given project.',
+    author='Rasul Kireev',
+    license='MIT',
+)
